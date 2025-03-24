e051f8cba427201d8938ac857bb1481035b5154f
Lucas Rodrigues <lucas.rodrigues@cargosapiens.com>
2025-03-24 17:22:39 -0300

    feat: Auth - By Eduardo

---
+++ backend/filmes/service.js
@@ -0,0 +1,66 @@
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const axios = require("axios");
+
+
+exports.getMovies = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/movie/popular?api_key=${ TMDB_API_KEY }&language=pt-BR`
+            );
+
+            resolve(response.data);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+
+exports.getMovieInfo = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log(" iouidasd");
+
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/movie/${ id }?api_key=${ TMDB_API_KEY }&language=pt-BR`
+            );
+
+            resolve(response.data);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+
+exports.searchMovies = (query, page = 1 ) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log(" iouidasd");
+
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${query}&page=${page}&language=pt-BR`
+            );
+            resolve(response.data);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+exports.moviesGenres = ( ) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/genre/movie/list?api_key=${TMDB_API_KEY}&language=pt-BR`
+            );
+
+            resolve(response.data);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Header({ searchQuery, setSearchQuery, handleSearchSubmit }) {
@@ -7,6 +7,19 @@ function Header({ searchQuery, setSearchQuery, handleSearchSubmit }) {
   // Função para alternar o menu de navegação
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+  }, []);
+
+  // Função para logout
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
   };
 
   return (
@@ -43,25 +56,42 @@ function Header({ searchQuery, setSearchQuery, handleSearchSubmit }) {
           <i className="fas fa-bars"></i> {/* Ícone de menu */}
         </button>
 
-        {/* Barra de pesquisa */}
-        <div className="relative">
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="px-6 py-3 w-96 text-lg rounded-full border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-300 ease-in-out"
-            placeholder="Pesquise por filmes..."
-          />
-          <button
-            onClick={handleSearchSubmit}
-            className="absolute right-2 top-1/2 transform -translate-y-1/2 px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 focus:outline-none transition duration-300"
-          >
-            Buscar
-          </button>
-        </div>
-      </div>
+        {  /* Barra de pesquisa */}
+          <div className="relative">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="px-6 py-3 w-96 text-lg rounded-full border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-300 ease-in-out"
+              placeholder="Pesquise por filmes..."
+            />
+            <button
+              onClick={handleSearchSubmit}
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 focus:outline-none transition duration-300"
+            >
+              Buscar
+            </button>
+          </div>
+          <div>
+            {isLoggedIn ? (
+              <button
+                onClick={handleLogout}
+                className="text-2xl font-bold text-white-250 hover:text-red-250 transition-all duration-300"
+              >
+                Logout
+              </button>
+            ) : (
+              <Link
+                to="/login"
+                className="text-2xl font-bold text-white-250 hover:text-red-250 transition-all duration-300"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+              </div>
 
-      {/* Menu de navegação para dispositivos pequenos */}
+              {/* Menu de navegação para dispositivos pequenos */}
       <div
         className={`md:hidden ${
           isMenuOpen ? "block" : "hidden"
